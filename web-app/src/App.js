eb8468681529874e0688bb03d04b3eca6b84cd54
Mathieu <mathieu.chauvin@eisti.fr>
2018-12-11 15:18:00 +0100

    add web3 provider for react

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -4,14 +4,19 @@ import logo from './logo.svg';
 import './App.css';
 import GridPro from './GridPro.js';
 import ProMenu from './ProMenu.js';
+import { Web3Provider } from 'react-web3';
+
 class App extends Component {
   render() {
 return (
+       
         <div className="app">
             <ProMenu/>
-            <Container style={{marginLeft:'auto',marginRight:'auto',maxWidth:'80%'}}>
-                <GridPro/>
-            </Container>
+            <Web3Provider>
+                <Container style={{marginLeft:'auto',marginRight:'auto',maxWidth:'80%'}}>
+                    <GridPro/>
+                </Container>
+            </Web3Provider>
         </div>
     );
   }
