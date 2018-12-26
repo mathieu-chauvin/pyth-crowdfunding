cf73969be64f016cbd745adb798cc903f5661991
Mathieu <mathieu.chauvin@eisti.fr>
2018-12-26 21:33:43 +0100

    add web3 detection

--- web-app/src/Grid.js
+++ web-app/src/Grid.js
@@ -4,10 +4,13 @@ import {Grid} from 'semantic-ui-react';
 import Miniature from './Miniature.js';
 
 class Grid extends Component {
+    const web3 = this.context.web3;
+
+
     render() {
         return (
+<p>{web3.selectedAccount}</p>
                 <Grid>
-
                     <Miniature/>
                     <Miniature/>
                     <Miniature/>
--- web-app/src/GridPro.js
+++ web-app/src/GridPro.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 
 import {Grid, Segment, Card} from 'semantic-ui-react';
 import Miniature from './Miniature.js';
-
+import PropTypes from 'prop-types';
 let GridElement = () =>  (
                 <Miniature />
         );
 
 class GridPro extends Component {
+
     render() {
-        return (
-                <Card.Group centered itemsPerRow={5} stackable>
+    const web3Context = this.context.web3;
+
+
+        return (<div>
+                <p>{JSON.stringify(this.context.web3)}</p><Card.Group centered itemsPerRow={5} stackable>
                     <GridElement/>
                     <GridElement/>
                     <GridElement/>
@@ -22,8 +26,13 @@ class GridPro extends Component {
                     <GridElement/>
                     <GridElement/>
                 </Card.Group>
-                   )
+                   </div>)
                 }
 }
+
+
+GridPro.contextTypes = {
+      web3: PropTypes.object
+};
 
 export default GridPro;
