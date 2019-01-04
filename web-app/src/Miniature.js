aacfcd4a556a2a4fac8df0e1952af8531ddcaf3b
Mathieu <mathieu.chauvin@eisti.fr>
2019-01-04 13:01:46 +0100

    show project info in miniature

--- web-app/src/Miniature.js
+++ web-app/src/Miniature.js
@@ -8,11 +8,11 @@ class Miniature extends Component {
                 <Card raised style={{margin:'20px',marginTop:'75px',textAlign:'center'}}>
                 <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' />
                 <Card.Content>
-                <Card.Header>Name of project</Card.Header>
+                <Card.Header>{this.props.project.name}</Card.Header>
                 <Card.Meta>
-                <span className='date'>17 décembre 2018</span>
+                <span className='date'></span>
                 </Card.Meta>
-                <Card.Description>Matthew is a musician living in Nashville. His cryptocurrency project is great !</Card.Description>
+                <Card.Description>{this.props.project.description}</Card.Description>
                 </Card.Content>
                 <Card.Content extra>
                 <b style={{textAlign:'center',fontSize:'28px'}}>10000 PTH</b><br/>
